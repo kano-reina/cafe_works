41c16906c9511ed518fadfab37857d3e4457728e
EC2 Default User <ec2-user@ip-172-31-15-55.ap-northeast-1.compute.internal>
2024-05-12 04:04:56 +0000

    05121304

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end 
+
+end
