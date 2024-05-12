e9d09969f510aa52de41ab9b9c1a1197b69a8a92
EC2 Default User <ec2-user@ip-172-31-15-55.ap-northeast-1.compute.internal>
2024-05-12 22:17:00 +0000

    05130716

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have successfully updated your account."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+  def withdraw
+    @user = User.find(params[:id])
+    if @user.update(user_params.merge(status: "withdrawn"))
+      flash[:notice] = "You have successfully deleted your account."
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -17,7 +38,7 @@ class UsersController < ApplicationController
   end 
 
   def user_params
-    params.require(:user).permit(:profile_image, :name)
+    params.require(:user).permit(:profile_image, :name, :email, :status)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   end
   root to: "homes#top"
   get 'about' => 'homes#about', as: 'about'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      patch :withdraw
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
