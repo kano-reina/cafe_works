a93b1770905d607de83a9f4179f43b1719e72cd9
EC2 Default User <ec2-user@ip-172-31-15-55.ap-northeast-1.compute.internal>
2024-05-14 22:17:27 +0000

    05150717

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  get 'about' => 'homes#about', as: 'about'
+  
   devise_for :users
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-  root to: "homes#top"
-  get 'about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update] do
     member do
       patch :withdraw
     end
   end
+  
+  resources :cafes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
