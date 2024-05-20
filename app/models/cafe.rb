999ef1f68ed943ed02bcda4dd9a6903d2214d4b8
EC2 Default User <ec2-user@ip-172-31-15-55.ap-northeast-1.compute.internal>
2024-05-20 22:18:49 +0000

    05210718

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -3,7 +3,12 @@ class Cafe < ApplicationRecord
   has_many :cafe_tags
   has_many :tags, through: :cafe_tags
   
-  
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :address, presence: true, uniqueness: { case_sensitive: false }
+  validates :city, presence: true
+  validates :has_power_outlet, presence: true
+  validates :chat_meeting_ok, presence: true
+  validates :has_wifi, presence: true
   
   def self.search_by_tags(tag_ids)
     joins(:cafe_tags).where(cafe_tags: { tag_id: tag_ids }).distinct
