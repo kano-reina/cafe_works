7936b49727c155a10fe371bbc1797e88fea1b3c1
EC2 Default User <ec2-user@ip-172-31-15-55.ap-northeast-1.compute.internal>
2024-06-02 05:08:45 +0000

    06021408

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,17 +1,31 @@
 class Public::CommentsController < ApplicationController
+  before_action :set_cafe
+  before_action :set_review
+
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @review.comments.new(comment_params)
     if @comment.save
-      redirect_to cafe_path(@comment.cafe), notice: 'Comment was successfully created.'
+      redirect_to cafe_path(@cafe), notice: 'Comment was successfully created.'
     else
-      redirect_to cafe_path(@comment.cafe), notice: 'Comment was not created.'
+      redirect_to cafe_path(@cafe), notice: 'Comment was not created.'
     end
   end
   
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to cafe_path(@cafe)
   end
   
   private
+  def set_cafe
+    @cafe = Cafe.find(params[:cafe_id])
+  end
+  
+  def set_review
+    @review = Review.find(params[:review_id])
+  end
+  
   def comment_params
     params.require(:comment).permit(:comment)
   end
